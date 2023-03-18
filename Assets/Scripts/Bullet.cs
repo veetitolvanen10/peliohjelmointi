898d9988eff8e38f034d5e95479b300b40c3814f
Veeti Tolvanen <veetitolvanen1@kamk.fi>
2023-03-19 01:17:14 +0200

    trig

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -24,7 +24,7 @@ public class Bullet : MonoBehaviour
                 {
             Destroy(gameObject);
             Destroy(Other.gameObject);
-            Debug.Log("Trigger!");
+            Debug.Log("Trig!");
         }
         
     }
